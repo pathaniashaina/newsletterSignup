b96c45bc45ba903aa3a9d35e3b4c1c2395d3e9f9
Shaina Pathania <pathaniashaina.22@gmail.com>
2021-02-24 16:48:33 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+const express=require("express");
+const request=require("request");
+const https=require("https");
+const app=express();
+
+
+// to use static files we must use this
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res){
+  const fname=req.body.fname;
+  const lname=req.body.lname;
+  const email=req.body.email;
+  const data={
+    members:[
+      {
+        email_address:email,
+        status:"subscribed",
+        merge_fields:{
+          FNAME:fname,
+          LNAME:lname,
+        }
+      }
+    ]
+  };
+const jsonData=JSON.stringify(data);
+
+const options={
+  method:"POST",
+  auth:process.env.APP_KEY,
+}
+
+const request=https.request(process.env.APP_ID,options,function(response){
+  response.on("data",function(data){
+    let d=JSON.parse(data);
+    console.log(d)
+  if((d.error_count)==0){
+    res.sendFile(__dirname+"/success.html");
+  }
+  else{
+      res.sendFile(__dirname+"/failure.html");
+  }
+  });
+
+});
+request.write(jsonData);
+request.end();
+});
+
+app.post("/failure",function(req,res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("server is running");
+});
